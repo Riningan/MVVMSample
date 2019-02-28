7dfd9f0a412b4a5fa439a34cd74eac7c865e844d
Riningan <Riningan@gmail.com>
2019-02-28 22:39:36 +0500

    develop

--- app/src/main/java/com/riningan/mvvmsample/presentation/MainActivity.kt
+++ app/src/main/java/com/riningan/mvvmsample/presentation/MainActivity.kt
@@ -1,21 +1,28 @@
-package com.riningan.mvvmsample
+package com.riningan.mvvmsample.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.riningan.mvvmsample.R
 import com.riningan.mvvmsample.presentation.pokemons_list.PokemonsListFragment
 
 
 class MainActivity : AppCompatActivity() {
+    var navController: NavController? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.navHostFragment)
     }
 
     override fun onStart() {
         super.onStart()
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction
-            .add(android.R.id.content, PokemonsListFragment())
-            .commit()
+                .add(android.R.id.content, PokemonsListFragment())
+                .commit()
     }
 }
